7a786c196f5982058aaad75893b0a06dab2366fa
Syed Samarul Huda <syed.samarul.huda@flyrobe.com>
2016-12-11 21:42:53 +0530

    First Commit

---
+++ app/src/main/java/com/example/samar/viewmoretextview/ui/MainActivity.java
@@ -0,0 +1,44 @@
+package com.example.samar.viewmoretextview.ui;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.text.Html;
+
+import com.example.samar.viewmoretextview.R;
+import com.example.samar.viewmoretextview.view.ExpandableTextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        initView();
+        initData();
+    }
+
+    public void initView()
+    {
+        String yourText = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. " +
+                "Ut volutpat interdum interdum. Nulla laoreet lacus diam, vitae " +
+                "sodales sapien commodo faucibus. Vestibulum et feugiat enim. Donec " +
+                "semper mi et euismod tempor. Sed sodales eleifend mi id varius. Nam " +
+                "et ornare enim, sit amet gravida sapien. Quisque gravida et enim vel " +
+                "volutpat. Vivamus egestas ut felis a blandit. Vivamus fringilla " +
+                "dignissim mollis. Maecenas imperdiet interdum hendrerit. Aliquam" +
+                " dictum hendrerit ultrices. Ut vitae vestibulum dolor. Donec auctor ante" +
+                " eget libero molestie porta. Nam tempor fringilla ultricies. Nam sem " +
+                "lectus, feugiat eget ullamcorper vitae, ornare et sem. Fusce dapibus ipsum" +
+                " sed laoreet suscipit. ";
+
+        ExpandableTextView expandableTextView = (ExpandableTextView) findViewById(R.id.tvText);
+        expandableTextView.setText(Html.fromHtml(yourText));
+
+    }
+
+    public void initData()
+    {
+
+    }
+}
